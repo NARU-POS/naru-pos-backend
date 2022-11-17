5b37908f0fa2b69edc6bfa6cf1b48cceb419a46f
handwoong <handwoong@gmail.com>
2022-11-17 20:34:29 +0900

    [#5] docs: 메뉴 컨트롤러 API swagger 문서 작성

--- src/api/menu.controller.ts
+++ src/api/menu.controller.ts
@@ -8,6 +8,14 @@ const menuController = Router();
 menuController.get(
     "/menus/category",
     wrapAsyncFunc(async (_req, res, _next) => {
+        /*
+            #swagger.tags = ["menu"]
+            #swagger.description = "메뉴 카테고리 조회"
+            #swagger.responses[200] = {
+                schema: { $ref: "#/definitions/getMenuCategoryResponse" },
+                description: "메뉴들의 카테고리와 세부 카테고리를 반환"
+            }
+         */
         const foundCategory = await MenuService.getCategoryList();
         res.status(STATUS_200_OK).json(foundCategory);
     }),
@@ -16,6 +24,26 @@ menuController.get(
 menuController.get(
     "/menus/:mainCategory/:detailCategory",
     wrapAsyncFunc(async (req, res, _next) => {
+        /*
+            #swagger.tags = ["menu"]
+            #swagger.description = "카테고리에 맞는 메뉴 조회"
+            #swagger.parameters["mainCategory"] = {
+                in: "path",
+                description: "조회할 메뉴의 메인 카테고리",
+                required: true,
+                schema: { $ref: "#/definitions/pathMainCategory" }
+            }
+            #swagger.parameters["detailCategory"] = {
+                in: "path",
+                description: "조회할 메뉴의 세부 카테고리",
+                required: true,
+                schema: { $ref: "#/definitions/pathDetailCategory" }
+            }
+            #swagger.responses[200] = {
+                schema: { $ref: "#/definitions/getMenuListResponse" },
+                description: "카테고리에 맞는 메뉴 목록을 반환"
+            }
+         */
         const { mainCategory, detailCategory } = req.params;
         const foundCategoryMenu = await MenuService.getCategoryMenu(mainCategory, detailCategory);
         res.status(STATUS_200_OK).json(foundCategoryMenu);
@@ -25,6 +53,20 @@ menuController.get(
 menuController.post(
     "/menus",
     wrapAsyncFunc(async (req, res, _next) => {
+        /*
+            #swagger.tags = ["menu"]
+            #swagger.description = "메뉴 생성"
+            #swagger.parameters["body"] = {
+                in: "body",
+                description: "생성하고자 하는 메뉴의 정보를 Request Body에 담아 요청",
+                required: true,
+                schema: { $ref: "#/definitions/postMenuRequest" }
+            }
+            #swagger.responses[200] = {
+                schema: { $ref: "#/definitions/postMenuResponse" },
+                description: "생성된 메뉴 정보를 반환"
+            }
+         */
         const createdMenu = await MenuService.addMenu(req.body);
         res.status(STATUS_201_CREATED).json(createdMenu);
     }),
@@ -33,6 +75,26 @@ menuController.post(
 menuController.put(
     "/menus/:menuId",
     wrapAsyncFunc(async (req, res, _next) => {
+        /*
+            #swagger.tags = ["menu"]
+            #swagger.description = "메뉴 수정"
+            #swagger.parameters["menuId"] = {
+                in: "path",
+                description: "수정하고자 하는 메뉴의 Id를 Request Path에 담아 요청",
+                required: true,
+                schema: { $ref: "#/definitions/pathPutMenu" }
+            }
+            #swagger.parameters["body"] = {
+                in: "body",
+                description: "수정하고자 하는 메뉴의 정보를 Request Body에 담아 요청",
+                required: true,
+                schema: { $ref: "#/definitions/putMenuRequest" }
+            }
+            #swagger.responses[200] = {
+                schema: { $ref: "#/definitions/putMenuResponse" },
+                description: "수정된 메뉴 정보를 반환"
+            }
+         */
         const { menuId } = req.params;
         const updatedMenu = await MenuService.updateMenu(menuId, req.body);
         res.status(STATUS_200_OK).json(updatedMenu);
@@ -42,6 +104,19 @@ menuController.put(
 menuController.delete(
     "/menus/:menuId",
     wrapAsyncFunc(async (req, res, _next) => {
+        /*  #swagger.tags = ["menu"]
+            #swagger.description = "메뉴 삭제"
+            #swagger.parameters["menuId"] = {
+                in: "path",
+                description: "삭제하고자 하는 메뉴의 Id를 Request Path에 담아 요청",
+                required: true,
+                schema: { $ref: "#/definitions/deleteMenuId" }
+            }
+            #swagger.responses[200] = {
+                schema: { "$ref": "#/definitions/deleteMenuResponse" },
+                description: "삭제 메시지를 반환"
+            }
+        */
         const { menuId } = req.params;
         const deletedMenu = await MenuService.deleteMenu(menuId);
         res.status(STATUS_200_OK).json(deletedMenu);
--- swagger.js
+++ swagger.js
@@ -28,6 +28,66 @@ const doc = {
             "beer & wine": ["unused"],
             tea: ["ice", "hot"],
         },
+        getMenuListResponse: [
+            {
+                status: "unused",
+                _id: "636cc0a64d85ca00dad95c18",
+                name: "아마트리치아나",
+                description: "고소한 베이컨과 매콤한 블랜딩 토마토 소스로 맛을 낸 로마식 파스타",
+                notice: "unused",
+                price: 12000,
+                category: "pasta",
+                detailCategory: "tomato",
+                spicy: 2,
+                photo_url:
+                    "https://res.cloudinary.com/dcahduceu/image/upload/v1668147672/naru/amatriciana.jpg",
+                __v: 0,
+            },
+        ],
+        pathMainCategory: "pasta",
+        pathDetailCategory: "tomato",
+        postMenuRequest: {
+            name: "수제레몬차(HOT)",
+            price: "5500",
+            detailCategory: "hot",
+            category: "tea",
+        },
+        postMenuResponse: {
+            name: "수제레몬차(HOT)",
+            description: "",
+            notice: "unused",
+            price: 5500,
+            category: "tea",
+            detailCategory: "hot",
+            status: "unused",
+            spicy: 0,
+            photo_url:
+                "https://res.cloudinary.com/dcahduceu/image/upload/v1663314125/naru/preparation.png",
+            _id: "637615eff40a91d94e608794",
+            __v: 0,
+        },
+        pathPutMenu: "637615eff40a91d94e608794",
+        putMenuRequest: {
+            price: 10000,
+        },
+        putMenuResponse: {
+            _id: "637615eff40a91d94e608794",
+            name: "수제레몬차(HOT)",
+            description: "",
+            notice: "unused",
+            price: 10000,
+            category: "tea",
+            detailCategory: "hot",
+            status: "unused",
+            spicy: 0,
+            photo_url:
+                "https://res.cloudinary.com/dcahduceu/image/upload/v1663314125/naru/preparation.png",
+            __v: 0,
+        },
+        deleteMenuId: "637615eff40a91d94e608794",
+        deleteMenuResponse: {
+            message: "삭제가 완료되었습니다.",
+        },
     },
 };
 
